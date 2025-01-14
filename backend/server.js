5ae8b4b5eed5a254806031822e293a95764739aa
harsh <Harshvyas98397916@gmail.com>
2025-01-14 18:36:38 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,80 @@
+import "dotenv/config";
+import http from "http";
 import app from "./app.js";
+import { Server } from "socket.io";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import projectModel from "./models/project.model.js"; // Import projectModel
 
-dotenv.config();
+const port = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI;
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
-if (!MONGO_URI) {
-  console.error("MongoDB connection string is missing in environment variables.");
-  process.exit(1);
-}
+io.use(async (socket, next) => {
+  try {
+    const token =
+      socket.handshake.auth?.token ||
+      socket.handshake.headers.authorization?.split(" ")[1];
+    const projectId = socket.handshake.query.projectId;
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    if (!mongoose.Types.ObjectId.isValid(projectId)) {
+      return next(new Error("Invalid projectId"));
+    }
+
+    socket.project = await projectModel.findById(projectId);
+
+    if (!token) {
+      return next(new Error("Authentication error"));
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded) {
+      return next(new Error("Authentication error"));
+    }
+
+    socket.user = decoded;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.join(socket.project._id.toString());
+
+  socket.on("project-message", (data) => {
+    console.log(data);
+    socket.broadcast
+      .to(socket.project._id.toString())
+      .emit("project-message", data);
+  });
+
+  socket.on("event", (data) => {
+    // Handle event
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+    server.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
     });
   })
   .catch((error) => {
